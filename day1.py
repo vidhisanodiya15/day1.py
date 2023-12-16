df66cf71da0bd0942e21765c28f93972c4c71cde
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2023-12-16 09:54:15 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,60 @@
+#print("hiie vidhi")
+#variables
+#types of variables
+#1.STRING
+#2.INTEGER
+#3.FLOATING
+#4.boolean(it checks the condition weahter it is true or false)
+#name="vidhi" (here name is a variable and vidhi is a string)
+#age= 18      (18 is a integer number)
+#print(age )
+
+#name="vijay"
+#age=24.13    (floating number)
+#print(name)
+#print(age)
+
+#is_adult=True
+
+#EXERCISE;
+#first_name="tony"
+#last_name="stark"
+#age= 51
+#print(first_name+" "+last_name)
+#is_genius= True
+#print(is_genius)
+
+#new
+#name=input("What is your name? ")
+#print(name)
+#print("hello"+name)  (this is known as concetanation)
+
+#EXERCISE;
+#superhero=input("what is your superhero name")
+#print(superhero)
+
+#new
+#old_age=input("enter your old age:")
+#new_age=int(old_age)+2   (here  we use int bcoz we are getting an error without int we can print the age it is called(type conversion))
+#print(new_age)
+
+#TYPE CONVERSION
+#1.INT
+#2.FLOAT
+#3.BOOL
+#4.STR
+
+ #NEW
+ 
+# first=input("enter first number:")
+# second=input("enter second number:")
+# sum= int(first)+int(second)
+# print(sum)
+
+
+#METHODS
+
+# name="Tony stark"
+# print(name.upper())
+
+# print(name)
